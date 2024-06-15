baac71e811be28506d3b924e397d6e1787258f4b
kunsc <congduyvu1997@gmail.com>
2024-06-15 20:56:18 +0700

    Exercise 15/06

---
+++ src/exercise_14/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package exercise_14;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        MultiplicationTableCalculation multiplication = new MultiplicationTableCalculation();
+        multiplication.CalculateMultiplication(5);
+    }
+}
---
+++ src/exercise_15/OddNaturalNumbers.java
@@ -0,0 +1,9 @@
+package exercise_15;
+
+public class OddNaturalNumbers {
+
+    public static void main(String[] args) {
+        OddNaturalNumbersCalculation oddNaturalNumbers = new OddNaturalNumbersCalculation();
+        oddNaturalNumbers.SumOddNaturalNumbers(10);
+    }
+}
