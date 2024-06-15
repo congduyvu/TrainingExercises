eb54dd6bae5938e51a0700a8aab7156c2a8cfbd9
congduyvu <congduyvu1997@gmail.com>
2024-06-16 06:17:19 +0700

    Task exercise 17, 18

---
+++ src/exercise_17/AngleTriangle.java
@@ -0,0 +1,9 @@
+package exercise_17;
+
+public class AngleTriangle {
+
+    public static void main(String[] args) {
+        AngleTriangleView angleTriangleView = new AngleTriangleView();
+        angleTriangleView.AssembleAngleTriangle(6);
+    }
+}
