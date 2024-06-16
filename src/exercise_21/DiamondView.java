9360b528599c0d917b520e8fde6020ee98647252
congduyvu <congduyvu1997@gmail.com>
2024-06-16 21:42:49 +0700

    Commit exercise 21

---
+++ src/exercise_21/DiamondView.java
@@ -0,0 +1,9 @@
+package exercise_21;
+
+public class DiamondView {
+
+    public static void main(String[] args) {
+        DiamondPattern diamondPattern = new DiamondPattern();
+        diamondPattern.AssembleDiamondPattern();
+    }
+}
