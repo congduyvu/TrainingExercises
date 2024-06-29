0c6bfd690d08676732c3bb2ff74803cfeada2f89
congduyvu <congduyvu1997@gmail.com>
2024-06-29 11:23:40 +0700

    Exercise29-06-2024

---
+++ src/exercise_24/NumberChecker.java
@@ -0,0 +1,9 @@
+package exercise_24;
+
+public class NumberChecker {
+
+    public static void main(String[] args) {
+        NumberCheckerView numberCheckerView = new NumberCheckerView();
+        numberCheckerView.CheckEqualNumbers(2345, 2345, 5555);
+    }
+}
