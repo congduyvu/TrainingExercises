22b2e476a7c74def62a3fe9099810eec6911cf3b
congduyvu <congduyvu1997@gmail.com>
2024-06-30 08:06:05 +0700

    Exercise 27 and 28

---
+++ src/exercise_27/NumbersDivision.java
@@ -0,0 +1,9 @@
+package exercise_27;
+
+public class NumbersDivision {
+
+    public static void main(String[] args) {
+        NumbersDivisionView numbersDivisionView = new NumbersDivisionView();
+        numbersDivisionView.DivideNumbersAndRoundUp(15, 4);
+    }
+}
---
+++ src/exercise_28/WholeFractionalParts.java
@@ -0,0 +1,9 @@
+package exercise_28;
+
+public class WholeFractionalParts {
+
+    public static void main(String[] args) {
+        WholeFractionalPartsView wholeFractionalPartsView = new WholeFractionalPartsView();
+        wholeFractionalPartsView.GetWholeAndFractionalPartOfNumber(666.9999);
+    }
+}
