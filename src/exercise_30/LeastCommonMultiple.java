c354f65593df32154f77854fbf8611597c400b18
congduyvu <congduyvu1997@gmail.com>
2024-07-03 19:49:54 +0700

    Add exercise 30

---
+++ src/exercise_30/LeastCommonMultiple.java
@@ -0,0 +1,9 @@
+package exercise_30;
+
+public class LeastCommonMultiple {
+
+    public static void main(String[] args) {
+        LeastCommonMultipleView leastCommonMultipleView = new LeastCommonMultipleView();
+        leastCommonMultipleView.GetLeastCommonMultiple(12, 18);
+    }
+}
